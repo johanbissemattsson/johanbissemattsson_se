e7e24b327fdc709a2fd86d8aa2e1a8ab7044a57f
johanbissemattsson <johan@johanbissemattsson.se>
2015-04-13 16:59:37 +0200

    Removed footer text

--- footer.php
+++ footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'johanbissemattsson' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'johanbissemattsson' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'johanbissemattsson' ), 'johanbissemattsson', '<a href="http://www.johanbissemattsson.se" rel="designer">Johan Bisse Mattsson</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
