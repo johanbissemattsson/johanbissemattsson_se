8b94f3901f5cbb3d0f8927033f00dfbb72b04e6d
johanbissemattsson <johan@johanbissemattsson.se>
2015-04-13 16:58:04 +0200

    Removed comments and sidebar

--- header.php
+++ header.php
@@ -27,6 +27,10 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'johanbissemattsson' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
